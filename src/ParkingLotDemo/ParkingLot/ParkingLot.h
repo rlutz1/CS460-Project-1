b3aec1094eb6eb05ace765f06db83fc86374e5b2
Roxanne Krause <rlutz1@unm.edu>
2026-02-22 18:29:04 -0700

    OKAY, figured out the slot mechanism and have a button communication back and forth!

--- src/ParkingLotDemo/ParkingLot/ParkingLot.h
+++ src/ParkingLotDemo/ParkingLot/ParkingLot.h
@@ -5,11 +5,12 @@
 #ifndef CS460_PROJECT_1_PARKINGLOT_H
 #define CS460_PROJECT_1_PARKINGLOT_H
 
+#include <QObject>
 #include <string>
 class Mediator;
 
 // namespace ParkingLot {
-class ParkingLot {
+class ParkingLot : public QObject {
 
 public:
     ParkingLot();
@@ -17,7 +18,8 @@ public:
 
     void add_output_stream(Mediator* pmc);
     void hardware_update(std::string str);
-    void send_signal();
+    public slots:
+        void send_signal();
 private:
     Mediator* mediator;
 };
--- src/ParkingLotDemo/run_parking_lot.cpp
+++ src/ParkingLotDemo/run_parking_lot.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <QApplication>
 #include <QPushButton>
+#include <QObject>
 #include "../run_demo.h"
 #include "Mediator/Mediator.h"
 #include "ParkingLot/ParkingLot.h"
 #include "PMSSoftware/ParkingManagementController.h"
 
 int run_parking_lot(int argc, char *argv[]) {
-    // QApplication a(argc, argv);
-    // QPushButton button("I am the parking lot application driver.", nullptr);
-    // button.resize(200, 100);
-    // button.show();
-    // return QApplication::exec();
+
     ParkingManagementController pmc;
-    ParkingLot pl;
-    Mediator mediator(&pl, &pmc);
+    ParkingLot parker;
+    Mediator mediator(&parker, &pmc);
     std::cout << "Parking Lot Making\n";
-    pl.add_output_stream(&mediator);
+    parker.add_output_stream(&mediator);
     pmc.add_output_stream(&mediator);
-    pl.send_signal();
-    return 0;
+    // pl.send_signal();
+
+    QApplication a(argc, argv);
+    QPushButton button("Sensor Trigger", nullptr);
+    button.resize(200, 100);
+    QObject::connect(&button, &QPushButton::clicked, &parker, &ParkingLot::send_signal);
+
+    button.show();
+
+
+
+
+    // return 0;
+
+    return QApplication::exec();
 }
