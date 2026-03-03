87b16f6e891c87400b1bd503363ae2150cf47b28
Gabriel Ramirez De Jesus <gramdj16@unm.edu>
2026-03-03 11:11:45 -0700

    Revert "fixed main to allow admin to run"
    This reverts commit d96f1757bdc4a82999db366486acbf2f7b85acda.

--- src/Admin/run_admin.cpp
+++ src/Admin/run_admin.cpp
@@ -1,15 +1,21 @@
-#include "PMS/Definitions"
-
 #include <QApplication>
 #include <QVBoxLayout>
 #include <QPushButton>
+
 #include <QComboBox>
+#include <QDebug>
 
 
 // driver for admin app, something strange
 int run_admin(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QWidget mainWindow;
+    QVBoxLayout layout(&mainWindow);
+    mainWindow.setLayout(&layout);
+    QPushButton button("admin app!", &mainWindow);
+    layout.addWidget(&button);
+    mainWindow.show();
+
     QComboBox *options = new QComboBox();
     options -> addItem("Make Unavailable");
     options -> addItem("Make Available");
@@ -25,6 +31,6 @@ int run_admin(int argc, char *argv[]) {
     // confirmLayout.addWidget(parking);
     confirmLayout.addWidget(&confirmButton);
     mainWindow.setLayout(&confirmLayout);
-    mainWindow.show();
+
     return QApplication::exec();
 }
